e589bdc807bda95f4725b1f7950c25e2fe6d7204
Husenmiyan Rafik Bobade <husenmiyan.bobade@gmail.com>
2026-08-24 14:38:33 +0530

    Day 19 - SQL Server integration, real DB connection via mssql, infra debugging

---
+++ db/database.js
@@ -0,0 +1,26 @@
+const sql = require('mssql');
+
+console.log('DEBUG - DB_SERVER seen by Playwright worker:', JSON.stringify(process.env.DB_SERVER));
+console.log('DEBUG - DB_PORT seen by Playwright worker:', JSON.stringify(process.env.DB_PORT));
+
+
+const config = {
+
+    server: process.env.DB_SERVER,
+    port: parseInt(process.env.DB_PORT),
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    options: {
+        encrypt: false,
+        trustServerCertificate: true
+    }
+};
+
+async function getConnection() {
+
+    return await sql.connect(config);
+    
+}
+
+module.exports = {sql, getConnection} ;
